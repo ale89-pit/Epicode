73a2081e5ecd3885130a9bd0ec4ae56b500e461f
ale89-pit <alessiop942@gmail.com>
2023-02-07 15:42:40 +0100

    commit

--- Mese_1/Settimana_2/Esercizio_G1/assets/js/secondo.js
+++ Mese_1/Settimana_2/Esercizio_G1/assets/js/secondo.js
@@ -1,3 +1,4 @@
+
 var animaleConDentoni = 'Criceto';
 var AnimaleColloLungo = 'Giraffa';
 var animale_Felino_1 = 'Leone';
@@ -6,11 +7,10 @@ var Animale_Felino_2 = 'Pantera';
 console.log(animaleConDentoni);
 console.log(AnimaleColloLungo);
 console.log(animale_Felino_1);
-
+document.getElementById('title').innerHTML = '<h1>Iniziamo la nostra pratica</h1>';
 
 document.getElementById('first').innerHTML = 'output scritto da Javascript' ;
 
 
 document.getElementById('second').innerHTML = Animale_Felino_2 + '' + 'scritto da javascript';
  
-document.getElementById('title').innerHTML = 'Iniziamo la nostra pratica';

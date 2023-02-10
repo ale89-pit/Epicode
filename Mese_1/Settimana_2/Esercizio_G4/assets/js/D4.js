d6a68bf1089cfdedf3fa6e7ac8828e4b5da4689a
ale89-pit <alessiop942@gmail.com>
2023-02-10 11:25:42 +0100

    La mia calcolatrice

--- Mese_1/Settimana_2/Esercizio_G4/assets/js/D4.js
+++ Mese_1/Settimana_2/Esercizio_G4/assets/js/D4.js
@@ -262,12 +262,14 @@ console.log(characters);
 /* EXTRA ESERCIZIO 10
 Crea una funzionalità che prenda un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo
 */
-var random = Math.floor(Math.random() * starWarsCharacters.length);
+
 
 console.log(random);
 
-
-  
+for(i = 0; i < starWarsCharacters.length; i++){
+  var random = Math.floor(Math.random() * starWarsCharacters.length);
   console.log(starWarsCharacters[random]);
-
+}
   
+ 
+
---
+++ Mese_1/Settimana_2/Esercizio_G5/assets/js/script.js
@@ -0,0 +1,81 @@
+let btn1 = document.getElementById('1');
+let btn2 = document.getElementById('2');
+let btn3 = document.getElementById('3');
+let btn4 = document.getElementById('4');
+let btn5 = document.getElementById('5');
+let btn6 = document.getElementById('6');
+let btn7 = document.getElementById('7');
+let btn8 = document.getElementById('8');
+let btn9 = document.getElementById('9');
+let btnDecimale = document.getElementById('.');
+let btnDivisione = document.getElementById('/');
+let btnMoltiplicazione = document.getElementById('*');
+let btnSottrazione = document.getElementById('-');
+let btnAddizione = document.getElementById('+');
+let btnOperazione = document.getElementById('operazione');
+let display = document.getElementById('text');
+let btnReset = document.getElementById('C');
+
+
+btn1.addEventListener('click', function(){
+    display.innerHTML += btn1.value ;
+})
+btn2.addEventListener('click', function(){
+    display.innerHTML += btn2.value ;
+})
+btn3.addEventListener('click', function(){
+    display.innerHTML += btn3.value ;
+})
+btn4.addEventListener('click', function(){
+    display.innerHTML += btn4.value ;
+})
+btn5.addEventListener('click', function(){
+    display.innerHTML += btn5.value ;
+})
+btn6.addEventListener('click', function(){
+    display.innerHTML += btn6.value ;
+})
+btn7.addEventListener('click', function(){
+    display.innerHTML += btn7.value ;
+})
+btn8.addEventListener('click', function(){
+    display.innerHTML += btn8.value ;
+})
+btn9.addEventListener('click', function(){
+    display.innerHTML += btn9.value ;
+})
+btnDivisione.addEventListener('click', function(){
+    display.innerHTML += btnDivisione.value ;
+})
+btnMoltiplicazione.addEventListener('click', function(){
+    display.innerHTML += btnMoltiplicazione.value ;
+})
+btnSottrazione.addEventListener('click', function(){
+    display.innerHTML += btnSottrazione.value ;
+})
+btnAddizione.addEventListener('click', function(){
+    display.innerHTML += btnAddizione.value ;
+})
+btnDecimale.addEventListener('click', function(){
+    display.innerHTML += btnDecimale.value ;
+})
+ 
+btnOperazione.addEventListener('click', function(){
+   display.innerText = eval(display.innerText);
+   display.innerHTML = display.innerText;
+    
+   
+  
+    
+     
+})
+
+btnReset.addEventListener('click', function(){
+    display.innerText = '' ; 
+})
+
+
+
+
+
+
